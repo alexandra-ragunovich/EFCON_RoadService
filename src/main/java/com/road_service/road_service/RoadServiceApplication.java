1d1ea81e7291ecc73b1f8d9ffb59f48c2be58f0b
alexandra <alexandra.ragunovich@gmail.com>
2026-03-18 20:27:19 +0300

    Initial commit

---
+++ src/main/java/com/road_service/road_service/RoadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.road_service.road_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/road_service/road_service/RoadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.road_service.road_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
